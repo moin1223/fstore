f326fb08b01a97cd45bcf7d802d5cf4801cf8787
moin1223 <moinpuccse@gmail.com>
2021-12-14 20:14:51 +0600

    text3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import AppRouter from './AppRouter';
 import Header from './components/common/Header';
 function App() {
-  
-  var hello = ''
-  console.log(hello);
+  let helloo = '';
+  console.log(helloo);
 
   return (
     <>
