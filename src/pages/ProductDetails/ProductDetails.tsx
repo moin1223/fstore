30b7257c92fc8bdf486a73aa16484add37e0bbf6
moin1223 <moinpuccse@gmail.com>
2021-12-18 21:26:25 +0600

    redux basic setup add

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -6,38 +6,46 @@ import { IProduct } from 'types';
 import imageUrlParser from 'utils/imageUrlParser';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from 'redux/actions/cartAction';
 interface IParams {
   id: string;
 }
-
 const ProductDetails = () => {
   const { id } = useParams<IParams>();
   const getProduct = useCallback(() => {
     return ProductService.getProductByID(id);
   }, [id]);
+
+  const dispatch = useDispatch();
+
   const { data, isLoading, isSuccess, isError, error } =
     useAsync<IProduct>(getProduct);
+
   const { name, image, description, price } = (data || {}) as IProduct;
   return (
-    <div className="product_details_component my-3">
+    <div className="product__details__component my-3">
       <Container>
         <div className="wrapper bg-white rounded border p-5">
-          {isLoading && <h3>Loading...</h3>}
+          {isLoading && <h3>Loading ....</h3>}
           {isSuccess && (
             <Row>
               <Col md={6}>
                 <img
                   className="img-fluid"
-                  src={imageUrlParser(data ? data.image : '')}
+                  src={imageUrlParser(data ? image : '')}
                   alt={name}
                 />
               </Col>
               <Col md={6}>
-                <h3 className="mb-3">{name}</h3>
-                <h1>৳ {price}</h1>
-                <button className="btn btn-primary">
+                <h3>{name}</h3>
+                <h1 className="mt-3 mb-5">৳ {price}</h1>
+                <button
+                  onClick={() => dispatch(addToCart(data as IProduct))}
+                  className="btn btn-primary"
+                >
                   <AiOutlineShoppingCart />
-                  Add to Card
+                  <span className="ms-2">Add to Cart</span>
                 </button>
                 <p className="mt-5">{description}</p>
               </Col>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,9 @@ import { createStore, combineReducers } from 'redux';
 import cartReducer from './reducers/cartReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  cart: cartReducer,
+});
 const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
