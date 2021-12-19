c4e1e86d048981a659a76c7964f38f84d8779761
moin1223 <moinpuccse@gmail.com>
2021-12-19 19:44:48 +0600

    [Add]  redux code  Refactor

---
+++ src/redux/actionCreators/cartAction.ts
@@ -0,0 +1,22 @@
+import { CartAction } from './../actions/cartAction';
+import { IProduct } from './../../types';
+import { ActionType } from 'redux/actionTypes';
+
+export const addToCart = (payload: IProduct): CartAction => {
+  return {
+    type: ActionType.ADD_TO_CART,
+    payload,
+  };
+};
+
+export const removeFromCart = (payload: string): CartAction => {
+  return {
+    type: ActionType.REMOVE_FROM_CART,
+    payload,
+  };
+};
+export const clearCart = (): CartAction => {
+  return {
+    type: ActionType.CLEAR_CART,
+  };
+};
